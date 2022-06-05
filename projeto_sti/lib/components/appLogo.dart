c25480c372778579f7ff1a4594f3955a3fc28f2e
Diego Andrés da Silva Briceño <diegobriceno13@gmail.com>
2022-06-05 19:16:37 +0100

    PC clicavel

--- projeto_sti/lib/components/appLogo.dart
+++ projeto_sti/lib/components/appLogo.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:projeto_sti/screens/mainScreen.dart';
 
 class AppLogo extends StatelessWidget {
   const AppLogo({
@@ -8,26 +9,33 @@ class AppLogo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 20.0, top: 0.0),
-      child: RichText(
-        text: TextSpan(
-          text: "P",
-          style: GoogleFonts.openSans(
-            fontSize: 55,
-            fontWeight: FontWeight.w900,
-            color: const Color.fromRGBO(47, 253, 246, 1),
-          ),
-          children: <TextSpan>[
-            TextSpan(
-              text: "C",
-              style: GoogleFonts.openSans(
-                fontSize: 55,
-                fontWeight: FontWeight.w900,
-                color: Colors.white,
-              ),
+    return GestureDetector(
+      onTap: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const MainScreen(),
+          )),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 20.0, top: 0.0),
+        child: RichText(
+          text: TextSpan(
+            text: "P",
+            style: GoogleFonts.openSans(
+              fontSize: 55,
+              fontWeight: FontWeight.w900,
+              color: const Color.fromRGBO(47, 253, 246, 1),
             ),
-          ],
+            children: <TextSpan>[
+              TextSpan(
+                text: "C",
+                style: GoogleFonts.openSans(
+                  fontSize: 55,
+                  fontWeight: FontWeight.w900,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
